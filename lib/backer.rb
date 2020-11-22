a4623de56843c3ed2bbe856f8820bfe932752a94
Conor Murnane <conormurnane11@gmail.com>
2020-11-22 15:51:52 -0500

    commit tree 52eb5f56751e47eac73d82aa35c29dc44fb414a9

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,18 +1,21 @@
 require 'pry'
 class Backer
     attr_accessor :name
-
+    #has many relationships do not hold the attributes of any other has many relationshp
     def initialize (name)
+        #initializes the backer's name and when it is in (argument) it is a value that will be passed upon the creation of the Class. To allow it to default simply assign the @name class variable in the method or name= to default value
         @name = name
     end
 
     def back_project(project)
         ProjectBacker.new(project,self)
+        #Calls upon the ProjectBacker class and creates a new instance. This instance takes in the argument of a project and can use self because it is being called in the Backer instance
+
     end
 
     def backed_projects
         
-       bck_projs = ProjectBacker.all.select{|name| name.backer == self}.map {|proj| proj.project}
+       ProjectBacker.all.select{|name| name.backer == self}.map {|proj| proj.project}
        #bck_projs.project
         
     end
--- lib/project_backer.rb
+++ lib/project_backer.rb
@@ -1,5 +1,6 @@
 class ProjectBacker
     attr_accessor :project, :backer
+    #belongs to class holds the attributes of the has many through relationships
     @@all = []
     def initialize(project,backer)
         @project = project
